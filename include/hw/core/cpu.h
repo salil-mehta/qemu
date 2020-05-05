043990515955020e85f5250e09f444049e98346a
Salil Mehta <salil.mehta@huawei.com>
2020-05-05 18:44:59 +0100

    arm/virt,kvm: Pre-create disabled possible vCPUs @machine init
    In the ARMv8 architecture, the GIC must know all the CPUs it is connected to
    during its initialization, and this cannot change afterward. This must be
    ensured during the initialization of the VGIC in KVM, which requires all vCPUs
    to be created and present during its initialization. This is necessary because:
    
    1. The association between GICC and MPIDR must be fixed at VM initialization
       time. This is represented by the register `GIC_TYPER(mp_affinity, proc_num)`.
    2. GICC (CPU interfaces), GICR (redistributors), etc., must all be initialized
       at boot time.
    3. Memory regions associated with GICR, etc., cannot be changed (added, deleted,
       or modified) after the VM has been initialized.
    
    This patch adds support to pre-create all possible vCPUs within the host using
    the KVM interface as part of the virtual machine initialization. These vCPUs can
    later be attached to QOM/ACPI when they are actually hot-plugged and made
    present.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Reported-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    [VP: Identified CPU stall issue & suggested probable fix]

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -530,6 +530,7 @@ struct CPUState {
     uint64_t dirty_pages;
     int kvm_vcpu_stats_fd;
     bool vcpu_dirty;
+    VMChangeStateEntry *vmcse;
 
     /* Use by accel-block: CPU is executing an ioctl() */
     QemuLockCnt in_ioctl_lock;
