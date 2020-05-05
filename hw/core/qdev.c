b2579d7566b1ab96cd02ff8757fff137d80ba0fe
Salil Mehta <salil.mehta@huawei.com>
2020-05-05 18:44:59 +0100

    arm/virt,kvm: Pre-create KVM vCPUs for 'disabled' QOM vCPUs at machine init
    ARM CPU architecture does not allow CPUs to be plugged after system has
    initialized. This is a constraint. Hence, the Kernel must know all the CPUs
    being booted during its initialization. This applies to the Guest Kernel as
    well and therefore, the number of KVM vCPU descriptors in the host must be
    fixed at VM initialization time.
    
    Also, the GIC must know all the CPUs it is connected to during its
    initialization, and this cannot change afterward. This must also be ensured
    during the initialization of the VGIC in KVM. This is necessary because:
    
    1. The association between GICR and MPIDR must be fixed at VM initialization
       time. This is represented by the register
       `GICR_TYPER(mp_affinity, proc_num)`.
    2. Memory regions associated with GICR, etc., cannot be changed (added,
       deleted, or modified) after the VM has been initialized. This is not an
       ARM architectural constraint but rather invites a difficult and messy
       change in VGIC data structures.
    
    To enable a hot-add–like model while preserving these constraints, the virt
    machine may enumerate more CPUs than are enabled at boot using
    `-smp disabledcpus=N`. Such CPUs are present but start offline (i.e.,
    administratively disabled at init). The topology remains fixed at VM
    creation time; only the online/offline status may change later.
    
    Administratively disabled vCPUs are not realized in QOM until first enabled,
    avoiding creation of unnecessary vCPU threads at boot. On large systems, this
    reduces startup time proportionally to the number of disabled vCPUs. Once a
    QOM vCPU is realized and its thread created, subsequent enable/disable actions
    do not unrealize it. This behaviour was adopted following review feedback and
    differs from earlier RFC versions.
    
    Co-developed-by: Keqian Zhu <zhuqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhuqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- hw/core/qdev.c
+++ hw/core/qdev.c
@@ -310,6 +310,23 @@ void qdev_assert_realized_properly(void)
                                    qdev_assert_realized_properly_cb, NULL);
 }
 
+bool qdev_disable(DeviceState *dev, BusState *bus, Error **errp)
+{
+    g_assert(dev);
+
+    if (bus) {
+        error_setg(errp, "Device %s 'disable' operation not supported",
+                   object_get_typename(OBJECT(dev)));
+        return false;
+    }
+
+    /* devices like cpu don't have bus */
+    g_assert(!DEVICE_GET_CLASS(dev)->bus_type);
+
+    return object_property_set_str(OBJECT(dev), "admin_power_state", "disabled",
+                                   errp);
+}
+
 bool qdev_machine_modified(void)
 {
     return qdev_hot_added || qdev_hot_removed;
