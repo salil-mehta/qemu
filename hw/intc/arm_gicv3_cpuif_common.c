f4658dbba518378d767d247c634681adb9404523
Salil Mehta <salil.mehta@huawei.com>
2020-05-06 02:20:23 +0100

    arm/virt, gicv3: Guard CPU interface access for admin disabled vCPUs
    Per Arm GIC Architecture Specification (IHI0069H_b, §11.1), the CPU interface
    and its Processing Element (PE) share a power domain. If the PE is powered down
    or administratively disabled, the CPU interface must be quiescent or off, and
    any access is architecturally UNPREDICTABLE. Without explicit checks, QEMU may
    issue GICC register operations for vCPUs that are offline, removed, or
    otherwise unavailable—risking inconsistent state or undefined behavior in both
    TCG and KVM accelerators.
    
    To address this, introduce a per-vCPU gicc_accessible flag that reflects the
    administrative enablement of the corresponding QOM vCPU in accordance with the
    policy. This is permissible when the GICC (GIC CPU Interface) is online-capable,
    meaning vCPUs can be brought online in the guest kernel after boot. The flag is
    set during GIC realization and used to skip VGIC register reads/writes, SGI
    generation, and CPU interface updates when the GICC is not accessible. This
    prevents unsafe operations and ensures compliance when managing administratively
    disabled but present vCPUs.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- hw/intc/arm_gicv3_cpuif_common.c
+++ hw/intc/arm_gicv3_cpuif_common.c
@@ -12,6 +12,9 @@
 #include "qemu/osdep.h"
 #include "gicv3_internal.h"
 #include "cpu.h"
+#include "qemu/log.h"
+#include "monitor/monitor.h"
+#include "qapi/visitor.h"
 
 void gicv3_set_gicv3state(CPUState *cpu, GICv3CPUState *s)
 {
@@ -21,6 +24,41 @@ void gicv3_set_gicv3state(CPUState *cpu, GICv3CPUState *s)
     env->gicv3state = (void *)s;
 };
 
+static void
+gicv3_get_gicc_accessibility(Object *obj, Visitor *v, const char *name,
+                             void *opaque, Error **errp)
+{
+    GICv3CPUState *cs = (GICv3CPUState *)opaque;
+    bool value = cs->gicc_accessible;
+
+    visit_type_bool(v, name, &value, errp);
+}
+
+static void
+gicv3_set_gicc_accessibility(Object *obj, Visitor *v, const char *name,
+                             void *opaque, Error **errp)
+{
+    GICv3CPUState *gcs = opaque;
+    CPUState *cs = gcs->cpu;
+    bool value;
+
+    visit_type_bool(v, name, &value, errp);
+
+    /* Block external attempts to set */
+    if (monitor_cur_is_qmp()) {
+        error_setg(errp, "Property 'gicc-accessible' is read-only externally");
+        return;
+    }
+
+    if (gcs->gicc_accessible != value) {
+        gcs->gicc_accessible = value;
+
+        qemu_log_mask(LOG_UNIMP,
+                      "GICC accessibility changed: vCPU %d = %s\n",
+                      cs->cpu_index, value ? "accessible" : "inaccessible");
+    }
+}
+
 void gicv3_init_cpuif(GICv3State *s)
 {
     ARMGICv3CommonClass *agcc = ARM_GICV3_COMMON_GET_CLASS(s);
@@ -28,6 +66,15 @@ void gicv3_init_cpuif(GICv3State *s)
 
     /* define and register `system registers` with the vCPU  */
     for (i = 0; i < s->num_cpu; i++) {
+        g_autofree char *propname = g_strdup_printf("gicc-accessible[%d]", i);
+        object_property_add(OBJECT(s), propname, "bool",
+                            gicv3_get_gicc_accessibility,
+                            gicv3_set_gicc_accessibility,
+                            NULL, &s->cpu[i]);
+
+        object_property_set_description(OBJECT(s), propname,
+            "Per-vCPU GICC interface accessibility (internal set only)");
+
         agcc->init_cpu_reginfo(s->cpu[i].cpu);
     }
 }
