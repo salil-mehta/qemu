2f5ccae2d938cb4a2dcec70cff83c25bae07cbd2
Salil Mehta <salil.mehta@huawei.com>
2020-05-06 14:12:34 +0100

    hw/arm/virt: Initialize PMUs for pre-created CPUs
    Initialize host PMU resources for CPUs in the possible-CPU list,
    including administratively disabled CPUs that are not yet realized.
    Iterating only the active CPU list leaves those CPUs uninitialized.
    
    Cache the machine's PMU setting and add helpers to iterate possible
    CPUs, skipping empty entries.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@opnsrc.net>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -178,6 +178,7 @@ struct VirtMachineState {
     bool mte;
     bool dtb_randomness;
     bool second_ns_uart_present;
+    bool pmu;
     OnOffAuto acpi;
     VirtGICType gic_version;
     VirtIOMMUType iommu;
