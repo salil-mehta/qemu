32fc9594a3d26b473b8cb2dc611e512df8795b46
Salil Mehta <salil.mehta@huawei.com>
2020-05-06 14:12:34 +0100

    arm/virt: Init PMU at host for all possible vCPUs
    The PMU for all possible vCPUs must be initialized during VM initialization.
    Refactor the existing code to accommodate possible vCPUs. This assumes that all
    processors being used are identical. It is an architectural constraint of ARM
    CPUs that all vCPUs MUST have identical feature sets, at least until the ARM
    specification is updated to allow otherwise.
    
    Past discussion for reference:
    Link: https://lists.gnu.org/archive/html/qemu-devel/2020-06/msg00131.html
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -161,6 +161,7 @@ struct VirtMachineState {
     bool mte;
     bool dtb_randomness;
     bool second_ns_uart_present;
+    bool pmu;
     OnOffAuto acpi;
     VirtGICType gic_version;
     VirtIOMMUType iommu;
