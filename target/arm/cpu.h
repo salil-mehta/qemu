6a78356f810e511d9920a65a3d9a6244912bec0d
Salil Mehta <salil.mehta@huawei.com>
2020-05-09 20:13:10 +0100

    target/arm: Add support of *unrealize* ARMCPU during vCPU Hot-unplug
    vCPU Hot-unplug will result in QOM CPU object unrealization which will do away
    with all the vCPU thread creations, allocations, registrations that happened
    as part of the realization process. This change introduces the ARM CPU unrealize
    function taking care of exactly that.
    
    Note, initialized KVM vCPUs are not destroyed in host KVM but their Qemu context
    is parked at the QEMU KVM layer.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Reported-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    [VP: Identified CPU stall issue & suggested probable fix]
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -1121,6 +1121,7 @@ struct ARMCPUClass {
 
     const ARMCPUInfo *info;
     DeviceRealize parent_realize;
+    DeviceUnrealize parent_unrealize;
     ResettablePhases parent_phases;
 };
 
@@ -3234,6 +3235,13 @@ static inline AddressSpace *arm_addressspace(CPUState *cs, MemTxAttrs attrs)
 void arm_register_pre_el_change_hook(ARMCPU *cpu, ARMELChangeHookFn *hook,
                                  void *opaque);
 /**
+ * arm_unregister_pre_el_change_hook:
+ * unregister all pre EL change hook functions. Generally called during
+ * unrealize'ing leg
+ */
+void arm_unregister_pre_el_change_hooks(ARMCPU *cpu);
+
+/**
  * arm_register_el_change_hook:
  * Register a hook function which will be called immediately after this
  * CPU changes exception level or mode. The hook function will be
@@ -3245,6 +3253,12 @@ void arm_register_pre_el_change_hook(ARMCPU *cpu, ARMELChangeHookFn *hook,
  */
 void arm_register_el_change_hook(ARMCPU *cpu, ARMELChangeHookFn *hook, void
         *opaque);
+/**
+ * arm_unregister_el_change_hook:
+ * unregister all EL change hook functions.  Generally called during
+ * unrealize'ing leg
+ */
+void arm_unregister_el_change_hooks(ARMCPU *cpu);
 
 /**
  * arm_rebuild_hflags:
