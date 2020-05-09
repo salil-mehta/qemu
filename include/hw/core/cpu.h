ca53ba7ae895ccdeac044a3a84539c7c6a86ecf1
Salil Mehta <salil.mehta@huawei.com>
2020-05-09 20:13:10 +0100

    arm/cpuhp: Add support of *unrealize* ARMCPU during vcpu hot-unplug
    During vcpu hot-unplug ARM cpu unrealization shall happen which should do away
    with all the vcpu thread creations, allocations, registrations which happened
    as part of the realization process of the ARM cpu. This change introduces the
    ARM cpu unrealize function taking care of exactly that.
    
    Note, initialized vcpus are not destroyed at host KVM but are rather parked in
    the QEMU/KVM layer. These are later reused once vcpu is hotplugged again.
    
    Co-developed-by: Salil Mehta <salil.mehta@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -365,6 +365,7 @@ struct CPUState {
     QSIMPLEQ_HEAD(, qemu_work_item) work_list;
 
     CPUAddressSpace *cpu_ases;
+    int cpu_ases_ref_count;
     int num_ases;
     AddressSpace *as;
     MemoryRegion *memory;
@@ -399,6 +400,7 @@ struct CPUState {
     struct kvm_dirty_gfn *kvm_dirty_gfns;
     uint32_t kvm_fetch_index;
     uint64_t dirty_pages;
+    VMChangeStateEntry *vmcse;
 
     /* Use by accel-block: CPU is executing an ioctl() */
     QemuLockCnt in_ioctl_lock;
