c0d722d21673cd101240ed71f61dd39f7d4d7886
Author Salil Mehta <salil.mehta@huawei.com>
2023-05-27 22:13:13 +0200

    target/arm/kvm,tcg: Handle SMCCC hypercall exits in VMM during PSCI_CPU_{ON,OFF}
    To support vCPU hotplug-like feature, we must trap any `HVC`/`SMC`
    `PSCI_CPU_{ON,OFF}` hypercalls from the host KVM to QEMU for policy checks. This
    ensures the following when a vCPU is brought online:
    
    1. The vCPU is actually plugged in (i.e., present).
    2. The vCPU is not administratively disabled. (Policy Checks)
    
    Implement the registration and handling of `HVC`/`SMC` hypercall exits within
    the VMM, ensuring that proper policy checks and control flow are enforced during
    the vCPU onlining and offlining processes.
    
    Co-developed-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- target/arm/kvm_arm.h
+++ target/arm/kvm_arm.h
@@ -192,6 +192,15 @@ bool kvm_arm_mte_supported(void);
  * Returns true if KVM can enable EL2 and false otherwise.
  */
 bool kvm_arm_el2_supported(void);
+
+/**
+ * kvm_arm_set_smccc_filter
+ * @func: funcion
+ * @faction: SMCCC filter action(handle, deny, fwd-to-user) to be deployed
+ *
+ * Sets the ARMs SMC-CC filter in KVM Host for selective hypercall exits
+ */
+int kvm_arm_set_smccc_filter(uint64_t func, uint8_t faction);
 #else
 
 static inline bool kvm_arm_aarch32_supported(void)
@@ -207,6 +216,11 @@ static inline bool kvm_arm_mte_supported(void)
 static inline bool kvm_arm_el2_supported(void)
 {
     return false;
+}
+
+static inline int kvm_arm_set_smccc_filter(uint64_t func, uint8_t faction)
+{
+    g_assert_not_reached();
 }
 #endif
 
