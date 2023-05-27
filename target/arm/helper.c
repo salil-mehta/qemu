2aa7b0c8cdd07086a345182b3a07acf5d74b93a7
Author Salil Mehta <salil.mehta@huawei.com>
2023-05-27 22:13:13 +0200

    target/arm/kvm,tcg: Handle SMCCC hypercall exits in VMM during PSCI_CPU_{ON,OFF}
    To support vCPU hotplug-like feature, we must trap any `HVC`/`SMC`
    `PSCI_CPU_{ON,OFF}` hypercalls from the host KVM to QEMU for policy checks. This
    ensures the following when a vCPU is brought online:
    
    1. The vCPU is actually plugged in (i.e., present).
    2. The vCPU is not administratively disabled. (Policy Checks)
    
    Implement the registration and handling of `HVC`/`SMC` hypercall exits within
    the VMM, ensuring that proper policy checks and control flow are enforced during
    the vCPU onlining and offlining processes.
    
    Co-developed-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- target/arm/helper.c
+++ target/arm/helper.c
@@ -9539,7 +9539,7 @@ void arm_cpu_do_interrupt(CPUState *cs)
                       env->exception.syndrome);
     }
 
-    if (tcg_enabled() && arm_is_psci_call(cpu, cs->exception_index)) {
+    if (arm_is_psci_call(cpu, cs->exception_index)) {
         arm_handle_psci_call(cpu);
         qemu_log_mask(CPU_LOG_INT, "...handled as PSCI call\n");
         qemu_plugin_vcpu_hostcall_cb(cs, last_pc);
