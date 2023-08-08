352318c2ae203204b255aadd8153d67ab1dd5e40
Salil Mehta <salil.mehta@huawei.com>
2023-08-08 00:43:18 +0000

    hw/acpi: Simulate *persistent* vCPU presence to Guest in ACPI _STA.{PRES,ENA} Bits
    Certain CPU architecture specifications [1][2][3] prohibit changes to the CPUs
    *presence* after the kernel has booted. This is because many system
    initializations depend on the exact CPU count at boot time and do not expect it
    to change afterward. For example, components like interrupt controllers that are
    closely coupled with CPUs, or various per-CPU features, may not support
    configuration changes once the kernel has been initialized.
    
    This requirement poses a challenge for virtualization features like vCPU
    hotplug. To address this, changes to the ACPI AML are necessary to update the
    `_STA.PRES` (presence) and `_STA.ENA` (enabled) bits accordingly during guest
    initialization, as well as when vCPUs are hot-plugged or hot-unplugged. The
    presence of unplugged vCPUs may need to be deliberately *simulated* at the ACPI
    level to maintain a *persistent* view of vCPUs for the guest kernel.
    
    Introduce an `acpi_persistent` property, which can be used to initialize the
    ACPI vCPU `presence` state accordingly. Architectures that require ACPI to
    expose a persistent view of vCPUs can override its default value. Refer to the
    subsequent patches for its usage.
    
    References:
    [1] KVMForum 2023 Presentation: Challenges Revisited in Supporting Virt CPU Hotplug on
        architectures that don’t Support CPU Hotplug (like ARM64)
        a. Kernel Link: https://kvm-forum.qemu.org/2023/KVM-forum-cpu-hotplug_7OJ1YyJ.pdf
        b. Qemu Link:  https://kvm-forum.qemu.org/2023/Challenges_Revisited_in_Supporting_Virt_CPU_Hotplug_-__ii0iNb3.pdf
    [2] KVMForum 2020 Presentation: Challenges in Supporting Virtual CPU Hotplug on
        SoC Based Systems (like ARM64)
        Link: https://kvmforum2020.sched.com/event/eE4m
    [3] Check comment 5 in the bugzilla entry
        Link: https://bugzilla.tianocore.org/show_bug.cgi?id=4481#c5
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- include/hw/acpi/cpu.h
+++ include/hw/acpi/cpu.h
@@ -26,6 +26,8 @@ typedef struct AcpiCpuStatus {
     uint64_t arch_id;
     bool is_inserting;
     bool is_removing;
+    bool is_present;
+    bool is_enabled;
     bool fw_remove;
     uint32_t ost_event;
     uint32_t ost_status;
@@ -92,5 +94,26 @@ static inline uint64_t acpi_get_cpu_archid(int cpu_index)
     assert((cpu_index >= 0) && (cpu_index < possible_cpus->len));
 
     return possible_cpus->cpus[cpu_index].arch_id;
+}
+
+/**
+ * acpi_persistent_cpu:
+ * @cpu: The vCPU to check
+ *
+ * Checks if the vCPU state should always be reflected as *present* via ACPI
+ * to the Guest. By default, this is False on all architectures and has to be
+ * explicity set during initialization.
+ *
+ * Returns: True if it is ACPI 'persistent' CPU
+ *
+ */
+static inline bool acpi_persistent_cpu(CPUState *cpu)
+{
+    assert(cpu);
+    /*
+     * returns if 'Presence' of the vCPU is persistent and should be simulated
+     * via ACPI even after vCPUs have been unplugged in QOM
+     */
+    return cpu->acpi_persistent;
 }
 #endif
