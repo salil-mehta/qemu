58b497a5d2cf821897cca69220b3766ad381783c
Salil Mehta <salil.mehta@huawei.com>
2023-08-08 00:43:18 +0000

    arm/virt: Make ARM vCPU *present* status ACPI *persistent*
    ARM arch does not allow CPUs presence to be changed [1] after kernel has booted.
    Hence, firmware/ACPI/Qemu must ensure persistent view of the vCPUs to the Guest
    kernel even when they are not present in the QoM i.e. are unplugged or are
    yet-to-be-plugged
    
    References:
    [1] Check comment 5 in the bugzilla entry
       Link: https://bugzilla.tianocore.org/show_bug.cgi?id=4481#c5
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3128,6 +3128,13 @@ static void virt_cpu_pre_plug(HotplugHandler *hotplug_dev, DeviceState *dev,
     if (dev->hotplugged) {
         virt_update_gic(vms, cs);
     }
+
+    /*
+     * To give persistent presence view of vCPUs to the guest, ACPI might need
+     * to fake the presence of the vCPUs to the guest but keep them disabled.
+     * This shall be used during the init of ACPI Hotplug state and hot-unplug
+     */
+    cs->acpi_persistent = true;
 }
 
 static void virt_cpu_plug(HotplugHandler *hotplug_dev, DeviceState *dev,
