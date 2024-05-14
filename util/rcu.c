salil-mehta/qemu

commit a464982499b2f637f6699e3d03e0a9d2e0b5288b

rcu: run RCU callbacks under the BQL
This needs to go away sooner or later, but one complication is the
complex VFIO data structures that are modified in instance_finalize.
Take a shortcut for now.

Reviewed-by: Michael Roth <mdroth@linux.vnet.ibm.com>
Tested-by: Michael Roth <mdroth@linux.vnet.ibm.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

util/rcu.c
@@ -35,6 +35,7 @@
 #include "qemu/rcu.h"
 #include "qemu/atomic.h"
 #include "qemu/thread.h"
+#include "qemu/main-loop.h"
 
 /*
  * Global grace period counter.  Bit 0 is always one in rcu_gp_ctr.
@@ -237,20 +238,24 @@ static void *call_rcu_thread(void *opaque)
 
         atomic_sub(&rcu_call_count, n);
         synchronize_rcu();
+        qemu_mutex_lock_iothread();
         while (n > 0) {
             node = try_dequeue();
             while (!node) {
+                qemu_mutex_unlock_iothread();
                 qemu_event_reset(&rcu_call_ready_event);
                 node = try_dequeue();
                 if (!node) {
                     qemu_event_wait(&rcu_call_ready_event);
                     node = try_dequeue();
                 }
+                qemu_mutex_lock_iothread();
             }
 
             n--;
             node->func(node);
         }
+        qemu_mutex_unlock_iothread();
     }
     abort();
 }

tests/Makefile
@@ -255,7 +255,7 @@ tests/test-x86-cpuid$(EXESUF): tests/test-x86-cpuid.o
 tests/test-xbzrle$(EXESUF): tests/test-xbzrle.o migration/xbzrle.o page_cache.o libqemuutil.a
 tests/test-cutils$(EXESUF): tests/test-cutils.o util/cutils.o
 tests/test-int128$(EXESUF): tests/test-int128.o
-tests/rcutorture$(EXESUF): tests/rcutorture.o libqemuutil.a
+tests/rcutorture$(EXESUF): tests/rcutorture.o libqemuutil.a libqemustub.a
 
 tests/test-qdev-global-props$(EXESUF): tests/test-qdev-global-props.o \
 	hw/core/qdev.o hw/core/qdev-properties.o hw/core/hotplug.o\

commit 6d966c5ad7b385d9dfc87b8ace8152f7522a2c8b

util/rcu: fix double free issue
When exercising vcpu hotplug with many hotplugs and unplugs the RCU
locking mechanism failed to protect the access to the node to dequeue.
Rearrange the lock/unlock procedure.

Fixes: a464982499b2 "rcu: run RCU callbacks under the BQL"
Signed-of-by: Miguel Luis <miguel.luis@oracle.com>

util/rcu.c
@@ -289,12 +289,14 @@ static void *call_rcu_thread(void *opaque)
             while (!node) {
                 bql_unlock();
                 qemu_event_reset(&rcu_call_ready_event);
+                bql_lock();
                 node = try_dequeue();
                 if (!node) {
+                    bql_unlock();
                     qemu_event_wait(&rcu_call_ready_event);
+                    bql_lock();
                     node = try_dequeue();
                 }
-                bql_lock();
             }
 
             n--;