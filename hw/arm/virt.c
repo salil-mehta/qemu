70762fbfca402af5291b7e9f2eab1acf081cb6d4
Salil Mehta <salil.mehta@huawei.com>
2024-06-10 17:43:09 +0000

    hw/arm/virt: Clamp 'maxcpus' as-per machine's vCPU deferred online-capability
    To support a vCPU hot-add–like model on ARM, the virt machine may be setup with
    more CPUs than are active at boot. These additional CPUs are fully realized in
    KVM and listed in ACPI tables from the start, but begin in a disabled state.
    They can later be brought online or taken offline under host or platform policy
    control. The CPU topology is fixed at VM creation time and cannot change
    dynamically on ARM. Therefore, we must determine precisely the 'maxcpus' value
    that applies for the full lifetime of the VM.
    
    On ARM, this deferred online-capable model is only valid if:
      - The GIC version is 3 or higher, and
      - Each non-boot CPU’s GIC CPU Interface is marked “online-capable” in its
        ACPI GICC structure (UEFI ACPI Specification 6.5, §5.2.12.14, Table 5.37
        “GICC CPU Interface Flags”), and
      - The chosen accelerator supports safe deferred CPU online:
          * TCG with multi-threaded TCG (MTTCG) enabled
          * KVM (on supported hosts)
          * Not HVF or QTest
    
    This patch sizes the machine’s max-possible CPUs during VM init:
      - If all conditions are satisfied, retain the full set of CPUs corresponding
        to (`-smp cpus` + `-smp disabledcpus`), allowing the additional (initially
        disabled) CPUs to participate in later policy-driven online.
      - Otherwise, clamp the max-possible CPUs to the boot-enabled count
        (`-smp disabledcpus=0` equivalent) to avoid advertising CPUs the guest can
        never use.
    
    A new MachineClass flag, `has_online_capable_cpus`, records whether the machine
    supports deferred vCPU online. This is usable by other machine types as well.
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2271,8 +2271,7 @@ static void machvirt_init(MachineState *machine)
     bool aarch64 = true;
     unsigned int smp_cpus = machine->smp.cpus;
     unsigned int max_cpus = machine->smp.max_cpus;
-
-    possible_cpus = mc->possible_cpu_arch_ids(machine);
+    DeviceClass *dc;
 
     /*
      * In accelerated mode, the memory map is computed earlier in kvm_type()
@@ -2289,7 +2288,7 @@ static void machvirt_init(MachineState *machine)
          * we are about to deal with. Once this is done, get rid of
          * the object.
          */
-        cpuobj = object_new(possible_cpus->cpus[0].type);
+        cpuobj = object_new(machine->cpu_type);
         armcpu = ARM_CPU(cpuobj);
 
         pa_bits = arm_pamax(armcpu);
@@ -2304,6 +2303,57 @@ static void machvirt_init(MachineState *machine)
      */
     finalize_gic_version(vms);
     finalize_msi_controller(vms);
+
+    /*
+     * The maximum number of CPUs depends on the GIC version, or on how
+     * many redistributors we can fit into the memory map (which in turn
+     * depends on whether this is a GICv3 or v4).
+     */
+     if (vms->gic_version == VIRT_GIC_VERSION_2) {
+        virt_max_cpus = GIC_NCPU;
+     } else {
+        virt_max_cpus = virt_redist_capacity(vms, VIRT_GIC_REDIST);
+        if (vms->highmem_redists) {
+            virt_max_cpus += virt_redist_capacity(vms, VIRT_HIGH_GIC_REDIST2);
+        }
+    }
+
+    if ((tcg_enabled() && !qemu_tcg_mttcg_enabled()) || hvf_enabled() ||
+        qtest_enabled() || vms->gic_version == VIRT_GIC_VERSION_2) {
+        max_cpus = machine->smp.max_cpus = smp_cpus;
+        if (mc->has_online_capable_cpus) {
+            if (vms->gic_version == VIRT_GIC_VERSION_2) {
+                warn_report("GICv2 does not support online-capable CPUs");
+            }
+            mc->has_online_capable_cpus = false;
+        }
+    }
+
+    if (mc->has_online_capable_cpus) {
+        max_cpus = smp_cpus + machine->smp.disabledcpus;
+        machine->smp.max_cpus = max_cpus;
+    }
+
+    if (max_cpus > virt_max_cpus) {
+        error_report("Number of SMP CPUs requested (%d) exceeds max CPUs "
+                     "supported by machine 'mach-virt' (%d)",
+                     max_cpus, virt_max_cpus);
+        if (vms->gic_version != VIRT_GIC_VERSION_2 && !vms->highmem_redists) {
+            error_printf("Try 'highmem-redists=on' for more CPUs\n");
+        }
+
+        exit(1);
+    }
+
+    dc = DEVICE_CLASS(object_class_by_name(machine->cpu_type));
+    if (!dc) {
+        error_report("CPU type '%s' not registered", machine->cpu_type);
+        exit(1);
+    }
+    dc->admin_power_state_supported = mc->has_online_capable_cpus;
+
+    /* uses smp.max_cpus to initialize all possible vCPUs */
+    possible_cpus = mc->possible_cpu_arch_ids(machine);
 
     if (vms->secure) {
         /*
@@ -2338,31 +2388,6 @@ static void machvirt_init(MachineState *machine)
         vms->psci_conduit = QEMU_PSCI_CONDUIT_SMC;
     } else {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_HVC;
-    }
-
-    /*
-     * The maximum number of CPUs depends on the GIC version, or on how
-     * many redistributors we can fit into the memory map (which in turn
-     * depends on whether this is a GICv3 or v4).
-     */
-    if (vms->gic_version == VIRT_GIC_VERSION_2) {
-        virt_max_cpus = GIC_NCPU;
-    } else {
-        virt_max_cpus = virt_redist_capacity(vms, VIRT_GIC_REDIST);
-        if (vms->highmem_redists) {
-            virt_max_cpus += virt_redist_capacity(vms, VIRT_HIGH_GIC_REDIST2);
-        }
-    }
-
-    if (max_cpus > virt_max_cpus) {
-        error_report("Number of SMP CPUs requested (%d) exceeds max CPUs "
-                     "supported by machine 'mach-virt' (%d)",
-                     max_cpus, virt_max_cpus);
-        if (vms->gic_version != VIRT_GIC_VERSION_2 && !vms->highmem_redists) {
-            error_printf("Try 'highmem-redists=on' for more CPUs\n");
-        }
-
-        exit(1);
     }
 
     if (vms->secure && !tcg_enabled() && !qtest_enabled()) {
@@ -3494,6 +3519,9 @@ static void virt_machine_class_init(ObjectClass *oc, const void *data)
     hc->plug = virt_machine_device_plug_cb;
     hc->unplug_request = virt_machine_device_unplug_request_cb;
     hc->unplug = virt_machine_device_unplug_cb;
+
+    mc->has_online_capable_cpus = true;
+
     mc->nvdimm_supported = true;
     mc->smp_props.clusters_supported = true;
     mc->auto_enable_numa_with_memhp = true;
