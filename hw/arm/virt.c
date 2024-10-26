18277ffc557556bbbc4800c6274dde82b2f32d9e
Salil Mehta <salil.mehta@huawei.com>
2024-10-26 01:47:35 +0000

    Revert "hw/arm/virt: remove GIC unwire"
    This reverts commit 6148af1583b438bb6996d08c693a78c7190327fd.

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -980,8 +980,8 @@ static void create_gic(VirtMachineState *vms, MemoryRegion *mem)
      * and the GIC's IRQ/FIQ/VIRQ/VFIQ/NMI/VINMI interrupt outputs to the
      * CPU's inputs.
      */
-    for (i = 0; i < max_cpus; i++) {
-        wire_gic_cpu_irqs(vms, qemu_get_possible_cpu(i));
+    for (i = 0; i < smp_cpus; i++) {
+        wire_gic_cpu_irqs(vms, qemu_get_cpu(i));
     }
 
     fdt_add_gic_node(vms);
@@ -3140,7 +3140,7 @@ static void virt_cpu_pre_plug(HotplugHandler *hotplug_dev, DeviceState *dev,
      */
     if (vms->acpi_dev) {
         virt_update_gic(vms, cs, true);
-        //wire_gic_cpu_irqs(vms, cs);
+        wire_gic_cpu_irqs(vms, cs);
     }
 
     if (!dev->hotplugged) {
@@ -3267,7 +3267,7 @@ static void virt_cpu_unplug(HotplugHandler *hotplug_dev, DeviceState *dev,
         goto fail;
     }
 
-    //unwire_gic_cpu_irqs(vms, cs);
+    unwire_gic_cpu_irqs(vms, cs);
     virt_update_gic(vms, cs, false);
 
     qemu_unregister_reset(do_cpu_reset, ARM_CPU(cs));
