0e629166fe7c6c9fd3da5bf3a7c7b465c0c296e9
Salil Mehta <salil.mehta@huawei.com>
2025-08-10 22:37:20 +0000

    hw/core, qemu-options.hx: Introduce 'disabledcpus' SMP parameter
    Add support for a new SMP configuration parameter, 'disabledcpus', which
    specifies the number of additional CPUs that are present in the virtual
    machine but administratively disabled at boot. These CPUs are visible in
    firmware (e.g. ACPI tables) yet unavailable to the guest until explicitly
    enabled via QMP/HMP, or via the 'device_set' API (introduced in later
    patches).
    
    This feature is intended for architectures such as ARM that lack native
    CPU hotplug support but can change the administrative power state of
    present CPUs. It allows simulating CPU hot-add–like scenarios while all
    CPUs remain physically present in the topology at boot time.
    
    Changes include:
     - Extend CpuTopology with a 'disabledcpus' field.
     - Update machine_parse_smp_config() to account for disabled CPUs when
       computing 'cpus' and 'maxcpus'.
     - Update SMPConfiguration in QAPI to accept 'disabledcpus'.
     - Extend -smp option documentation to describe 'disabledcpus' usage and
       behavior.
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- system/vl.c
+++ system/vl.c
@@ -737,6 +737,9 @@ static QemuOptsList qemu_smp_opts = {
             .name = "cpus",
             .type = QEMU_OPT_NUMBER,
         }, {
+            .name = "disabledcpus",
+            .type = QEMU_OPT_NUMBER,
+        }, {
             .name = "drawers",
             .type = QEMU_OPT_NUMBER,
         }, {
