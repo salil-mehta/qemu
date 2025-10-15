b3dd65469fdcdec478cc8d541458fb6593609121
Salil Mehta <salil.mehta@opnsrc.net>
2025-10-15 22:24:39 +0000

    hw/arm/virt: Kick the vCPU after it is lazily realized
    When a vCPU is administratively enabled and realized, it starts with
    CPUState::stopped = true (via qemu_init_vcpu()). The first transition to running
    occurs when the VM is started: vm_start() calls vcpus_resume_all(), which clears
    stopped and kicks the threads. Lazily realized vCPUs remain in the stopped state
    and must be explicitly resumed, which leads to setting CPUState::stopped = false
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2006,6 +2006,8 @@ virt_cpu_pre_poweron(PowerStateHandler *handler, DeviceState *dev, Error **errp)
      */
     if (!dev->realized) {
         qdev_realize(dev, NULL, errp);
+        /* qemu_init_vcpu() sets CPUState::stopped=true; resume now */
+        cpu_resume(cs);
     } else {
         /* Realized but parked 'disabled' vCPUs */
         virt_unpark_cpu_in_userspace(cs);
