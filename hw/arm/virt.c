e352646afe895c1c19130a7d87719f861d229961
Salil Mehta <salil.mehta@opnsrc.net>
2025-10-22 18:26:58 +0000

    target/arm: Finalize CPU features before creating host vCPU
    When lazily creating host vCPUs for administratively disabled CPUs,
    kvm_arm_create_host_vcpu() currently calls kvm_arch_init_vcpu() without first
    finalizing the CPU’s architectural feature set. This can lead to a crash on
    platforms such as Nvidia Grace-Hopper, where SVE or SME are supported by
    default, because the vector length and related feature state remain
    uninitialized at the time of KVM_ARM_VCPU_INIT.
    
    In the normal realization path, arm_cpu_finalize_features() is always invoked
    before initializing the KVM vCPU. This patch restores that ordering for the
    lazy-realization path as well, ensuring that feature descriptors, including SVE
    and SME configuration, are finalized prior to any KVM ioctls.
    
    With this change, the lazy vCPU creation flow becomes:
    machvirt_init()
       virt_setup_lazy_vcpu_realization()
           arm_cpu_finalize_features()
           kvm_arm_create_host_vcpu()
               kvm_create_vcpu()
               kvm_arch_init_vcpu()
                   kvm_arm_vcpu_finalize(...)
    
    This matches the existing realization sequence and avoids early-init crashes or
    inconsistent feature programming when the option "-smp disabledcpus=N" is used
    on systems with advanced CPU features like SVE etc.
    
    Reported-by: Gavin Shan <gshan@redhat.com>
    [Link: https://lore.kernel.org/qemu-devel/a03ed205-b61a-4bba-9f25-83663b7d8a86@redhat.com/]
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    (cherry picked from commit 49c4676c2b9425b8aa8b7367189cfbb5663d3fd1)
    (cherry picked from commit dce2bbb6b0b0cb5e9e9cb042e9c48fb0b6aeebe9)
    (cherry picked from commit cb58a581fd4092c9aac75160389e957956dc0cd2)

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3148,6 +3148,9 @@ virt_setup_lazy_vcpu_realization(Object *cpuobj, VirtMachineState *vms)
     /* set operational state of disabled CPUs as OFF */
     ARM_CPU(cpuobj)->power_state = PSCI_OFF;
 
+    /* finalize the features like SVE, SME etc */
+    arm_cpu_finalize_features(ARM_CPU(cpuobj), &error_fatal);
+
     /*
      * [!] Constraint: The ARM CPU architecture does not permit new CPUs
      * to be added after system initialization.
