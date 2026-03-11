81838ee18342cce6d19d2fae47b875bb6f369c18
Salil Mehta <salil.mehta@huawei.com>
2026-03-11 20:39:06 +0000

    cpuhp: changes -v7-rc1.4.1

--- hw/acpi/ich9.c
+++ hw/acpi/ich9.c
@@ -454,7 +454,7 @@ void ich9_pm_device_plug_cb(HotplugHandler *hotplug_dev, DeviceState *dev,
                                 dev, errp);
         }
     } else if (object_dynamic_cast(OBJECT(dev), TYPE_CPU)) {
-        acpi_cpu_plug_cb(hotplug_dev, &lpc->pm.cpuhp_state, dev, errp);
+        acpi_cpu_plug_cb(DEVICE(lpc), &lpc->pm.cpuhp_state, dev, errp);
     } else if (object_dynamic_cast(OBJECT(dev), TYPE_PCI_DEVICE)) {
         acpi_pcihp_device_plug_cb(hotplug_dev, &lpc->pm.acpi_pci_hotplug,
                                   dev, errp);
@@ -486,7 +486,7 @@ void ich9_pm_device_unplug_request_cb(HotplugHandler *hotplug_dev,
             return;
         }
 
-        acpi_cpu_unplug_request_cb(hotplug_dev, &lpc->pm.cpuhp_state,
+        acpi_cpu_unplug_request_cb(DEVICE(lpc), &lpc->pm.cpuhp_state,
                                    dev, errp);
     } else if (object_dynamic_cast(OBJECT(dev), TYPE_PCI_DEVICE)) {
         acpi_pcihp_device_unplug_request_cb(hotplug_dev,
