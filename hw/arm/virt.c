22c76c70f1768aa5a19b2f6a9c3b3d8004acb68e
Salil Mehta <salil.mehta@opnsrc.net>
2026-05-30 21:58:23 +0000

    hw/arm/virt: revert the disabledcpus in virt

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3018,11 +3018,6 @@ static void machvirt_init(MachineState *machine)
         }
     }
 
-    if (mc->has_online_capable_cpus) {
-        max_cpus = smp_cpus + machine->smp.disabledcpus;
-        machine->smp.max_cpus = max_cpus;
-    }
-
     if (max_cpus > virt_max_cpus) {
         error_report("Number of SMP CPUs requested (%d) exceeds max CPUs "
                      "supported by machine 'mach-virt' (%d)",
