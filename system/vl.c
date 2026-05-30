0464e238a570d4e17313e579a4b2a270cfee0b2d
Salil Mehta <salil.mehta@opnsrc.net>
2026-05-30 21:55:06 +0000

    Revert "hw/core, qemu-options.hx: Introduce 'disabledcpus' SMP parameter"
    This reverts commit b9817470058980afe476085bfc90d36cdb23aac4.

--- system/vl.c
+++ system/vl.c
@@ -739,9 +739,6 @@ static QemuOptsList qemu_smp_opts = {
             .name = "cpus",
             .type = QEMU_OPT_NUMBER,
         }, {
-            .name = "disabledcpus",
-            .type = QEMU_OPT_NUMBER,
-        }, {
             .name = "drawers",
             .type = QEMU_OPT_NUMBER,
         }, {
